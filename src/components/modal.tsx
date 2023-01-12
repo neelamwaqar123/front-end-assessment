373edca7837205d9449a797328954b037d98cdd5
neelamwaqar123 <neelamwaqar37@gmail.com>
2023-01-13 02:46:54 +0500

    feat(Spinner):Add loading animation to indicate content is loading

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -5,6 +5,7 @@ import {
   Button,
   useDisclosure,
   Box,
+  Spinner,
 } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
@@ -15,12 +16,18 @@ import { SolidChild, SizeObject } from "../interface";
 import TextContent from "./TextContent";
 
 function ContentModal() {
+  const [modalActive, setModalActive] = useState(false);
   const [selectedSize, setSelectedSize] = useState("");
   const [price, setPrice] = useState("");
   const [selectedImage, setSelectedImage] = useState(
     productDummyData?.solid[0].images[0]
   );
   const { onOpen, onClose, isOpen } = useDisclosure({ defaultIsOpen: true });
+
+  //imitating api for showing spinner
+  useEffect(() => {
+    setTimeout(() => setModalActive(true), 2000);
+  }, []);
 
   useEffect(() => {
     let firstActive =
@@ -38,177 +45,182 @@ function ContentModal() {
     price && setPrice(price);
   }, []);
 
-  return (
-    <>
-      {/* <Button onClick={onOpen}>Open Modal</Button> */}
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalContent
-          maxW={["75vw", "380px", "580px", "985px", "990px"]}
-          h={["auto", "auto", "auto", "500px", "500px"]}
-          borderRadius="0px"
+  return !modalActive ? (
+    <Spinner
+      size={["lg", "lg", "xl", "xl", "xl"]}
+      position="fixed"
+      top="50%"
+      left="50%"
+      transform="translate(-50%, -50%)"
+    />
+  ) : (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalContent
+        maxW={["75vw", "380px", "580px", "985px", "990px"]}
+        h={["auto", "auto", "auto", "500px", "500px"]}
+        borderRadius="0px"
+      >
+        <ModalBody
+          p={["20px", "20px", "20px", "20px", "20px"]}
+          display="flex"
+          flexDirection={["column", "column", "column", "row", "row"]}
+          h="inherit"
         >
-          <ModalBody
-            p={["20px", "20px", "20px", "20px", "20px"]}
+          <TextContent
+            name={productDummyData.name}
+            description={productDummyData.description}
+            type={productDummyData.type}
+            price={price}
+            isMobile
+          />
+          <Box
+            className="left"
             display="flex"
-            flexDirection={["column", "column", "column", "row", "row"]}
-            h="inherit"
+            w={["100%", "100%", "100%", "650px", "650px"]}
+            h={["auto", "390px", "610px", "inherit", "inherit"]}
+            justifyContent={[
+              "space-between",
+              "space-between",
+              "space-between",
+              "unset",
+              "unset",
+            ]}
+            flexDirection={[
+              "column-reverse",
+              "column-reverse",
+              "column-reverse",
+              "row",
+              "row",
+            ]}
+          >
+            <Box
+              display="flex"
+              flexDirection={["row", "row", "row", "column", "column"]}
+              maxH={["70px", "90px", "115px", "450px", "450px"]}
+              h={["70px", "90px", "115px", "unset", "unset"]}
+              w={["100%", "100%", "100%", "150px", "150px"]}
+              maxW={["100%", "100%", "100%", "150px", "150px"]}
+              overflow="auto"
+              flexWrap="nowrap"
+              justifyContent="start"
+              alignItems="center"
+            >
+              {productDummyData &&
+                productDummyData?.solid.map((solid: SolidChild) =>
+                  solid.images.map((img: string, index: number) => (
+                    <Image
+                      cursor="pointer"
+                      w={["52px", "58px", "98px", "80px", "80px"]}
+                      h={["52px", "58px", "98px", "80px", "80px"]}
+                      m={["0 5px", "0 5px", "0 5px", "5px", "5px"]}
+                      src={img}
+                      key={index}
+                      onClick={() => setSelectedImage(img)}
+                    />
+                  ))
+                )}
+            </Box>
+            <Box
+              className="product-img"
+              w="100%"
+              h={["100%", "100%", "100%", "460px", "460px"]}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Image
+                m="0 auto"
+                src={selectedImage}
+                w={["90%", "90%", "90%", "100%", "100%"]}
+                h={["100%", "100%", "unset", "100%", "100%"]}
+              />
+            </Box>
+          </Box>
+          <Box
+            className="right"
+            ml={["unset", "unset", "unset", "20px", "20px"]}
+            h={[
+              "fit-content",
+              "fit-content",
+              "fit-content",
+              "inherit",
+              "inherit",
+            ]}
+            w={["100%", "100%", "100%", "350px", "350px"]}
           >
             <TextContent
               name={productDummyData.name}
               description={productDummyData.description}
               type={productDummyData.type}
               price={price}
-              isMobile
+              isMobile={false}
             />
             <Box
-              className="left"
+              className="actions"
+              pt={["20px", "20px", "10px", "20px", "20px"]}
               display="flex"
-              w={["100%", "100%", "100%", "650px", "650px"]}
-              h={["auto", "390px", "610px", "inherit", "inherit"]}
               justifyContent={[
+                "flex-start",
+                "flex-start",
                 "space-between",
-                "space-between",
-                "space-between",
-                "unset",
-                "unset",
+                "flex-start",
+                "flex-start",
               ]}
-              flexDirection={[
-                "column-reverse",
-                "column-reverse",
-                "column-reverse",
-                "row",
-                "row",
-              ]}
+              h={["inherit", "inherit", "inherit", "320px", "320px"]}
+              w={["100%", "100%", "100%", "100%", "100%"]}
+              flexDirection={["column", "column", "row", "column", "column"]}
             >
-              <Box
-                display="flex"
-                flexDirection={["row", "row", "row", "column", "column"]}
-                maxH={["70px", "90px", "115px", "450px", "450px"]}
-                h={["70px", "90px", "115px", "unset", "unset"]}
-                w={["100%", "100%", "100%", "150px", "150px"]}
-                maxW={["100%", "100%", "100%", "150px", "150px"]}
-                overflow="auto"
-                flexWrap="nowrap"
-                justifyContent="start"
-                alignItems="center"
-              >
-                {productDummyData &&
-                  productDummyData?.solid.map((solid: SolidChild) =>
-                    solid.images.map((img: string, index: number) => (
-                      <Image
-                        cursor="pointer"
-                        w={["52px", "58px", "98px", "80px", "80px"]}
-                        h={["52px", "58px", "98px", "80px", "80px"]}
-                        m={["0 5px", "0 5px", "0 5px", "5px", "5px"]}
-                        src={img}
-                        key={index}
-                        onClick={() => setSelectedImage(img)}
-                      />
-                    ))
-                  )}
-              </Box>
-              <Box
-                className="product-img"
-                w="100%"
-                h={["100%", "100%", "100%", "460px", "460px"]}
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Image
-                  m="0 auto"
-                  src={selectedImage}
-                  w={["90%", "90%", "90%", "100%", "100%"]}
-                  h={["100%", "100%", "unset", "100%", "100%"]}
-                />
-              </Box>
-            </Box>
-            <Box
-              className="right"
-              ml={["unset", "unset", "unset", "20px", "20px"]}
-              h={[
-                "fit-content",
-                "fit-content",
-                "fit-content",
-                "inherit",
-                "inherit",
-              ]}
-              w={["100%", "100%", "100%", "350px", "350px"]}
-            >
-              <TextContent
-                name={productDummyData.name}
-                description={productDummyData.description}
-                type={productDummyData.type}
-                price={price}
-                isMobile={false}
-              />
-              <Box
-                className="actions"
-                pt={["20px", "20px", "10px", "20px", "20px"]}
-                display="flex"
-                justifyContent={[
-                  "flex-start",
-                  "flex-start",
-                  "space-between",
-                  "flex-start",
-                  "flex-start",
-                ]}
-                h={["inherit", "inherit", "inherit", "320px", "320px"]}
-                w={["100%", "100%", "100%", "100%", "100%"]}
-                flexDirection={["column", "column", "row", "column", "column"]}
-              >
-                <Box display="flex" flexDirection="column">
-                  <Text
-                    fontWeight={600}
-                    fontSize={["18px", "18px", "18px", "20px", "20px"]}
-                  >
-                    Size
-                  </Text>
-                  <Box
-                    flexWrap="wrap"
-                    className="sizes"
-                    display="flex"
-                    justifyContent="space-between"
-                  >
-                    {productDummyData &&
-                      productDummyData?.solid.map((solid: any) =>
-                        solid.size?.map(
-                          (size: ProductProperty, index: number) => (
-                            <ProductSize
-                              key={index}
-                              size={Object.keys(size)[0]}
-                              price={Object.values(size)[0]?.price}
-                              index={index}
-                              setSelectedSize={setSelectedSize}
-                              setPrice={setPrice}
-                              active={Object.keys(size)[0] === selectedSize}
-                            />
-                          )
-                        )
-                      )}
-                  </Box>
-                </Box>
-                <Box
-                  mt={["10px", "10px", "auto", "auto", "auto"]}
-                  h={["40px", "40px", "40px", "50px", "50px"]}
-                  w={["100%", "100%", "50%", "100%", "100%"]}
+              <Box display="flex" flexDirection="column">
+                <Text
+                  fontWeight={600}
+                  fontSize={["18px", "18px", "18px", "20px", "20px"]}
                 >
-                  <Button
-                    h="inherit"
-                    w="100%"
-                    color="#ffffff"
-                    background="#DF1E1C"
-                    borderRadius="0px"
-                  >
-                    Add to Cart
-                  </Button>
+                  Size
+                </Text>
+                <Box
+                  flexWrap="wrap"
+                  className="sizes"
+                  display="flex"
+                  justifyContent="space-between"
+                >
+                  {productDummyData &&
+                    productDummyData?.solid.map((solid: any) =>
+                      solid.size?.map(
+                        (size: ProductProperty, index: number) => (
+                          <ProductSize
+                            key={index}
+                            size={Object.keys(size)[0]}
+                            price={Object.values(size)[0]?.price}
+                            index={index}
+                            setSelectedSize={setSelectedSize}
+                            setPrice={setPrice}
+                            active={Object.keys(size)[0] === selectedSize}
+                          />
+                        )
+                      )
+                    )}
                 </Box>
               </Box>
+              <Box
+                mt={["10px", "10px", "auto", "auto", "auto"]}
+                h={["40px", "40px", "40px", "50px", "50px"]}
+                w={["100%", "100%", "50%", "100%", "100%"]}
+              >
+                <Button
+                  h="inherit"
+                  w="100%"
+                  color="#ffffff"
+                  background="#DF1E1C"
+                  borderRadius="0px"
+                >
+                  Add to Cart
+                </Button>
+              </Box>
             </Box>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
+          </Box>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   );
 }
 
